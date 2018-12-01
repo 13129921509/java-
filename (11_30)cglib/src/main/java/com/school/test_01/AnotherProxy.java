fb2ae38bfbfc35066c468b75b42d20ca22d5102d
13129921509 <942445346@qq.com>
2018-12-01 20:39:42 +0800

    12-1 two

--- (11_30)cglib/src/main/java/com/school/test_01/AnotherProxy.java
+++ (11_30)cglib/src/main/java/com/school/test_01/AnotherProxy.java
@@ -5,11 +5,13 @@ import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 import java.text.DateFormat;
+import java.util.Date;
 
 public class AnotherProxy implements MethodInterceptor{
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        System.out.println("start:"+DateFormat.getDateInstance());
+        System.out.println("start:"+DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.LONG).format(new Date()));
         methodProxy.invokeSuper(o,objects);
-        System.out.println("end:"+DateFormat.getDateInstance());
+        System.out.println("end:"+DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.LONG).format(new Date()));
+        return null;
     }
 }
--- (11_30)cglib/src/main/java/com/school/test_01/Test_01.java
+++ (11_30)cglib/src/main/java/com/school/test_01/Test_01.java
@@ -1,6 +1,8 @@
 package com.school.test_01;
 
+import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
 
 public class Test_01 {
     public static void main(String[] args) {
@@ -8,8 +10,11 @@ public class Test_01 {
         AnotherProxy daoProxy2 = new AnotherProxy();
         Enhancer enhaner = new Enhancer();
         enhaner.setSuperclass(Dao.class);
-        enhaner.setCallback(daoProxy);
+        enhaner.setCallbacks(new Callback[]{daoProxy,daoProxy2, NoOp.INSTANCE});
+        enhaner.setCallbackFilter(new ProxyFilter());
+        enhaner.setInterceptDuringConstruction(false);
         Dao dao = (Dao) enhaner.create();
         dao.update();
+        dao.select();
     }
 }
